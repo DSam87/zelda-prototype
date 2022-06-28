8ee347d8be4c119bd917b7f051de8b5771fae8d2
DSam87 <76795390+DSam87@users.noreply.github.com>
2022-06-28 15:02:03 -0500

    Fixed flickering player movement

--- Zelda_Prototype/Assets/Scripts/PlayerController.cs
+++ Zelda_Prototype/Assets/Scripts/PlayerController.cs
@@ -8,42 +8,102 @@ public class PlayerController : MonoBehaviour
     public Animator theAnim;
     public float playerSpeed;
 
+    public float movementCountDown;
+    public float countDownDuration;
+
 
     // Start is called before the first frame update
     void Start()
     {
         theRB = GetComponent<Rigidbody2D>();
         theAnim = GetComponent<Animator>();
-
+        movementCountDown = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if(Input.GetAxisRaw("Horizontal") != 0)
-        {
-            theAnim.SetFloat("HorizontalMove", Input.GetAxisRaw("Horizontal"));
-            theAnim.SetFloat("VerticalMove", 0f);     
-            theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), 0f) * playerSpeed;
-        }
-
-        if(Input.GetAxisRaw("Vertical") != 0)
-        {
-            theAnim.SetFloat("VerticalMove", Input.GetAxisRaw("Vertical"));
-            theAnim.SetFloat("HorizontalMove", 0f);               
-            theRB.velocity = new Vector2(0f, Input.GetAxisRaw("Vertical")) * playerSpeed;            
-        }
-
-        if(Input.GetAxisRaw("Vertical") != 0 && Input.GetAxisRaw("Horizontal") != 0)
-        {
-            theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * playerSpeed * .7f;   
-        }
-
-        if(Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") == 0)
+        if(movementCountDown <= 0)
         {
             theRB.velocity = new Vector2(0, 0);  
+            movementCountDown = countDownDuration;
+
+            if(Input.GetAxisRaw("Horizontal") != 0)
+            {
+                if(Input.GetAxisRaw("Horizontal") > 0)
+                {
+                    if(Input.GetAxisRaw("Vertical") == 0)
+                    {
+                        theAnim.SetFloat("lastPosition", 1);
+                    }
+                    else
+                    {
+                        if(Input.GetAxisRaw("Vertical") > 0)
+                        {
+                            theAnim.SetFloat("lastPosition", 0);
+                        }
+                        else
+                        {
+                            theAnim.SetFloat("lastPosition", 2);
+                        }
+                    }
+                }
+                else
+                {
+                    if(Input.GetAxisRaw("Vertical") == 0)
+                    {
+                        theAnim.SetFloat("lastPosition", 3);
+                    }
+                }
+                theAnim.SetFloat("HorizontalMove", Input.GetAxisRaw("Horizontal"));
+                theAnim.SetFloat("VerticalMove", 0f);
+                theAnim.SetBool("isMoving", true);
+                theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), 0f) * playerSpeed;
+            }
+
+            if(Input.GetAxisRaw("Vertical") != 0)
+            {
+                if(Input.GetAxisRaw("Vertical") > 0)
+                {
+                    theAnim.SetFloat("lastPosition", 0);
+                }
+                else
+                {
+                    theAnim.SetFloat("lastPosition", 2);
+                }
+                theAnim.SetFloat("VerticalMove", Input.GetAxisRaw("Vertical"));
+                theAnim.SetFloat("HorizontalMove", 0f);        
+                theAnim.SetBool("isMoving", true);       
+                theRB.velocity = new Vector2(0f, Input.GetAxisRaw("Vertical")) * playerSpeed;            
+            }
+
+            if(Input.GetAxisRaw("Vertical") != 0 && Input.GetAxisRaw("Horizontal") != 0)
+            {
+
+                if(Input.GetAxisRaw("Vertical") > 0)
+                {
+                    theAnim.SetFloat("lastPosition", 0);
+                }
+                else
+                {
+                    theAnim.SetFloat("lastPosition", 2);
+                }
+                theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * playerSpeed * .7f;   
+                theAnim.SetBool("isMoving", true);
+            }
+
+            if(Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") == 0)
+            {
+                theRB.velocity = new Vector2(0, 0);  
+                theAnim.SetBool("isMoving", false);
+            }
+        }
+        else
+        {
+           movementCountDown -= Time.deltaTime;
         }
 
     }
+
+
 }
